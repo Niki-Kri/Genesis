e4a598e1cc747787c60350882e5b5285e3e1b89e
Niki Kumari <2024ugcs073@gmail.com>
2026-04-01 20:20:13 +0530

    Added root route and fixed backend for render

--- Backend/server.js
+++ Backend/server.js
@@ -5,23 +5,27 @@ import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", chatRoutes);
-
-app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`);
-    connectDB();
+app.get("/", (req, res) => {
+  res.send("Genesis backend is running");
 });
 
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected with Database!");
-    } catch(err) {
-        console.log("Failed to connect with Db", err);
-    }
-}
+app.use("/api", chatRoutes);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected with Database!");
+  } catch (error) {
+    console.log("Failed to connect with Db", error);
+  }
+};
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+  connectDB();
+});
